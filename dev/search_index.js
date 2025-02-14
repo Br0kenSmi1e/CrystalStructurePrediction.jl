77df53a4ad360f8368227a75bab14c85748a6592
Documenter.jl <documenter@juliadocs.github.io>
2025-02-14 06:11:16 +0000

    build based on abaac8e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CrystalStructurePrediction","category":"page"},{"location":"#CrystalStructurePrediction","page":"Home","title":"CrystalStructurePrediction","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CrystalStructurePrediction.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CrystalStructurePrediction]","category":"page"}]
+}
