72841680e2140fecbe50cb1c100494054a320564
LongliZheng <longlizheng2024@163.com>
2025-02-25 08:48:08 +0800

    test passed

--- test/interaction.jl
+++ test/interaction.jl
@@ -7,8 +7,8 @@ using CrystalStructurePrediction
 end
 
 @testset "real_space_sum" begin
-    ion_a = Ion("", rand(3), 1)
-    ion_b = Ion("", rand(3), 2)
+    ion_a = Ion("", 1, rand(3))
+    ion_b = Ion("", 2, rand(3))
     lattice = Lattice(rand(3,3), [true, true, true])
     alpha = 2.0
     for depth in range(0,4)
@@ -18,8 +18,8 @@ end
 end
 
 @testset "reciprocal_space_sum" begin
-    ion_a = Ion("", rand(3), 1)
-    ion_b = Ion("", rand(3), 2)
+    ion_a = Ion("", 1, rand(3))
+    ion_b = Ion("", 2, rand(3))
     lattice = Lattice(rand(3,3), [true, true, true])
     alpha = 2.0
     for depth in range(0,4)
@@ -29,8 +29,8 @@ end
 end
 
 @testset "buckingham_sum" begin
-    ion_a = Ion("Sr", rand(3), 1)
-    ion_b = Ion("O", rand(3), 2)
+    ion_a = Ion("Sr", 1, rand(3))
+    ion_b = Ion("O", 2, rand(3))
     lattice = Lattice(rand(3,3), [true, true, true])
     for depth in range(0,4)
         depth_list = [depth for _ in range(1,3)]
