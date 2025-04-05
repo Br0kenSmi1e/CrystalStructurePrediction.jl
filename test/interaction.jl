794209f47af96640917d72a65ef4914398718981
GiggleLiu <cacate0129@gmail.com>
2025-04-05 19:53:18 +0800

    ave

--- test/interaction.jl
+++ test/interaction.jl
@@ -38,3 +38,14 @@ end
         @test buckingham_sum(ion_a, ion_b, lattice, depth_list) ≈ buckingham_sum(ion_b, ion_a, lattice, depth_list)
     end
 end
+
+@testset "interaction_energy" begin
+    lattice = Lattice([1.0 0.0 0.0; 0.0 0.8 0.0; 0.0 0.6 1.2], (true, true, true))
+    ion_a = Ion(IonType(:O, -2, 1.35), [1.0, 0.5, 0.5])
+    ion_b = Ion(IonType(:O, -2, 1.35), [0.0, 0.0, 0.2])
+    alpha = 2.0 / (abs(det(lattice.vectors)))^(1/3)
+    depth = (4, 4, 4)
+    @test interaction_energy(ion_a, ion_b, lattice, alpha, depth, depth, depth) ≈ interaction_energy(ion_b, ion_a, lattice, alpha, depth, depth, depth)
+    @test interaction_energy(ion_a, ion_b, lattice, alpha, depth, depth, depth) ≈ 322.4479207721009
+end
+
